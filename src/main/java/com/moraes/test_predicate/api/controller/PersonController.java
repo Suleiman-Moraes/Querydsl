4259e63c640df9587dcb9883aed8f71c0a63debf
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-08-07 11:46:13 -0300

    Insert SpringFox Swagger

--- src/main/java/com/moraes/test_predicate/api/controller/PersonController.java
+++ src/main/java/com/moraes/test_predicate/api/controller/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.moraes.test_predicate.api.model.Person;
 import com.moraes.test_predicate.api.service.PersonService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
@@ -16,6 +18,7 @@ public class PersonController {
 	@Autowired
 	private PersonService service;
 	
+	@ApiOperation(value = "test querydsl")
 	@GetMapping
 	public ResponseEntity<Person> test(){
 		return ResponseEntity.ok(service.test());

62415243eaaeb25114e39b4e84805d7b1f8769db
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-08-11 17:01:57 -0300

    ajuste

--- src/main/java/com/moraes/test_predicate/TestPredicateApplication.java
+++ src/main/java/com/moraes/test_predicate/TestPredicateApplication.java
@@ -1,13 +1,29 @@
 package com.moraes.test_predicate;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
-@SpringBootApplication
-public class TestPredicateApplication {
+@SpringBootApplication(scanBasePackages = { "com.moraes.test_predicate" })
+public class TestPredicateApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestPredicateApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestPredicateApplication.class);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() { 
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
 }
